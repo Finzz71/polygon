a0442b0ddb11ad883570e53a179107cb8eb5664c
Finzz71 <timonyakytsk@gmail.com>
2025-01-17 23:48:28 +0300

    выполнил вариант 13

---
+++ lab9.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def longest_sequence(arr):
+
+    # Вычисляет разницу между соседними элементами массива
+    # Затем сравнивает каждую разницу с нулем, результат — булев массив
+    equal_neighbors = np.diff(arr) == 0
+
+    # Объединяем массивы в один. 
+    # Выделяем нулем начало и конец каждой последовательности одинаковых элементов.
+    equal_neighbors = np.concatenate(([0], equal_neighbors, [0]))
+
+    # Преобразует булев массив в числовой
+    changes = np.diff(equal_neighbors.astype(int))
+    starts = np.where(changes == 1)[0]
+    ends = np.where(changes == -1)[0]
+
+    # Вычисляем длины каждой последовательности одинаковых элементов как разницу между индексами концов и начал
+    lengths = ends - starts + 1
+    max_length = lengths.max() if len(lengths) > 0 else 1
+
+    return max_length
+
+# Example usage
+array = np.array([1, 1, 1, 2, 2, 5])
+print("Longest sequence length:", longest_sequence(array))
